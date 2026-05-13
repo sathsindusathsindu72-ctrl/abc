b31c3a0f8f59a01afdc82e538e39a449b448d54b
lalanidilhani34-glitch <lalanidilhani34@gmail.com>
2026-05-13 11:32:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=65f7f3e63e0dc0ea' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=fa2040e4167ea43e' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
