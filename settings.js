e7213759896109eab34840e8583740c222e52302
lalanidilhani34-glitch <lalanidilhani34@gmail.com>
2026-05-16 09:00:34 +0530

    Update SESSION_ID default value in settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=9bcff7f4467ba199' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=8f13c9833820e1d0' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
